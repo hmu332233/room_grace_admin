1a39814adb354a7a98df7b631114597d07f6c045
minung.han <hmu332233@gmail.com>
2019-08-16 01:43:15 +0900

    refactor: 참고용 Counter component와 redux 로직 제거

--- src/client/store/modules/index.js
+++ src/client/store/modules/index.js
@@ -1,8 +1,6 @@
 import { combineReducers } from 'redux';
-import counter from './counter';
 import timePicker from './timePicker';
 
 export default combineReducers({
-  counter,
   timePicker,
 });
