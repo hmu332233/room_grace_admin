92f4781ea4c783c4625d7a1265825071e61d1bff
minung.han <hmu332233@gmail.com>
2019-08-15 16:35:41 +0900

    feat: switch와 redux 로직 추가

--- src/client/containers/TimeSelector/TimeSelector.jsx
+++ src/client/containers/TimeSelector/TimeSelector.jsx
@@ -3,10 +3,9 @@ import styles from './TimeSelector.scss';
 import 'rc-time-picker/assets/index.css';
 
 import { connect } from 'react-redux';
-import { addTime } from 'store/modules/timePicker';
+import { addTime, toggleRunning } from 'store/modules/timePicker';
 
-import TimePicker from 'rc-time-picker';
-import { Button } from 'antd';
+import { TimePicker, Switch , Button } from 'antd';
 
 class TimeSelector extends React.Component {
   constructor(props) {
@@ -24,11 +23,16 @@ class TimeSelector extends React.Component {
     this.state.time && this.props.addTime(this.state.time);
   }
 
+  handleSwitchChange = () => {
+    this.props.toggleRunning();
+  }
+
   render() {
     return (
       <div className={styles.TimeSelector}>
+        <Switch checked={this.props.isRunning} onChange={this.handleSwitchChange} />
         <TimePicker
-          showSecond={false}
+          format={'HH:mm'}
           onChange={this.handleTimeChange}
         />
         <Button type="primary" onClick={this.handleButtonClick}>추가</Button>
@@ -38,10 +42,12 @@ class TimeSelector extends React.Component {
 }
 
 const mapStateToProps = ({ timePicker }) => ({
+  isRunning: timePicker.isRunning,
 });
 
 const mapDispatchToProps = dispatch => ({
   addTime: time => dispatch(addTime(time)),
+  toggleRunning: () => dispatch(toggleRunning())
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(TimeSelector);
--- src/client/store/modules/timePicker.js
+++ src/client/store/modules/timePicker.js
@@ -1,15 +1,23 @@
 const ADD = 'timePicker/ADD';
 const DELETE = 'timePicker/DELETE';
+const TOGGLE_RUNNING = 'timePicker/TOGGLE_RUNNING';
 
+export const toggleRunning = () => ({ type: TOGGLE_RUNNING });
 export const addTime = time => ({ type: ADD, time });
 export const deleteTime = index => ({ type: DELETE, index });
 
 const initialState = {
+  isRunning: false,
   times: [],
 };
 
 export default function timePicker(state = initialState, action) {
   switch (action.type) {
+    case TOGGLE_RUNNING: 
+      return {
+        ...state,
+        isRunning: !state.isRunning,
+      };
     case ADD:
       return {
         ...state,
