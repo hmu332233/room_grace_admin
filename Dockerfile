602a936f3ded9348f1effd912d26c2163a7aa80a
minung.han <hmu332233@gmail.com>
2020-01-25 23:42:13 +0900

    feat: docker에서 돌아가기 위한 일부 설정 추가

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,10 @@ MAINTAINER minung.han <hmu332233@gmail.com>
 RUN mkdir -p /app
 WORKDIR /app
 
+# 필수 라이브러리들 설치
+RUN apt-get update && \
+    apt-get install -y gconf-service libasound2 libatk1.0-0 libatk-bridge2.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget
+
 # 앱 의존성 설치
 COPY package*.json ./
 RUN  npm install --production
@@ -19,5 +23,8 @@ ENV NODE_ENV production
 # 타임존 변경
 RUN bash -c 'echo "Asia/Seoul" > /etc/timezone' && rm /etc/localtime && \
     dpkg-reconfigure -f noninteractive tzdata
+
+# apt-get 리스트 제거
+RUN rm -rf /var/lib/apt/lists/*
 
 CMD npm run start:server
--- src/server/utils/jnuPoster.js
+++ src/server/utils/jnuPoster.js
@@ -1,26 +1,20 @@
-const puppeteer = require("puppeteer");
+const puppeteer = require('puppeteer');
 
-const LOGIN_URL = "https://portal.jnu.ac.kr/Common/Login/Login.aspx";
-const WRITE_PAGE_URL = "http://www.jnu.ac.kr/WebApp/web/HOM/COM/Board/board.aspx?boardID=10&bbsMode=write&page=1";
+const LOGIN_URL = 'https://portal.jnu.ac.kr/Common/Login/Login.aspx';
+const WRITE_PAGE_URL = 'http://www.jnu.ac.kr/WebApp/web/HOM/COM/Board/board.aspx?boardID=10&bbsMode=write&page=1';
 const USER_NAME = '원룸그레이스';
 
 exports.post = async ({ title, userName = USER_NAME, contents, id, pw }) => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
   const page = await browser.newPage();
 
   // 로그인
   await page.goto(LOGIN_URL);
   await page.evaluate(
     (id, pw) => {
-      document.getElementById(
-        "ContentPlaceHolder1_LoginUser_UserName"
-      ).value = id;
-      document.getElementById(
-        "ContentPlaceHolder1_LoginUser_Password"
-      ).value = pw;
-      document
-        .getElementById("ContentPlaceHolder1_LoginUser_LoginButton")
-        .click();
+      document.getElementById('ContentPlaceHolder1_LoginUser_UserName').value = id;
+      document.getElementById('ContentPlaceHolder1_LoginUser_Password').value = pw;
+      document.getElementById('ContentPlaceHolder1_LoginUser_LoginButton').click();
     },
     id,
     pw
@@ -33,12 +27,8 @@ exports.post = async ({ title, userName = USER_NAME, contents, id, pw }) => {
   // 제목과 이름 작성
   await page.evaluate(
     (title, userName) => {
-      document.getElementById(
-        "ctl00_ctl00_ContentPlaceHolder1_PageContent_ctl00_txt_boardTitle"
-      ).value = title;
-      document.getElementById(
-        "ctl00_ctl00_ContentPlaceHolder1_PageContent_ctl00_txt_boardWriter"
-      ).value = userName;
+      document.getElementById('ctl00_ctl00_ContentPlaceHolder1_PageContent_ctl00_txt_boardTitle').value = title;
+      document.getElementById('ctl00_ctl00_ContentPlaceHolder1_PageContent_ctl00_txt_boardWriter').value = userName;
     },
     title,
     userName
@@ -46,17 +36,17 @@ exports.post = async ({ title, userName = USER_NAME, contents, id, pw }) => {
   await page.waitFor(1000);
 
   // 소스코드 클릭
-  await page.click("#cke_18");
+  await page.click('#cke_18');
 
   await page.waitFor(500);
 
   // 소스코드 작성
   await page.evaluate(contents => {
-    document.querySelector("#cke_1_contents > textarea").value = contents;
+    document.querySelector('#cke_1_contents > textarea').value = contents;
   }, contents);
 
   // 작성 완료 버튼 클릭 및 브라우저 종료
-  await page.click("#ctl00_ctl00_ContentPlaceHolder1_PageContent_ctl00_btnSave");
+  await page.click('#ctl00_ctl00_ContentPlaceHolder1_PageContent_ctl00_btnSave');
   await page.waitFor(1000);
   await browser.close();
 
