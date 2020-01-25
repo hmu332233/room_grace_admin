84c2dd8898c3acb0e5b90a86fd4e37d844376872
minung.han <hmu332233@gmail.com>
2020-01-25 22:20:42 +0900

    feat: cron.post에서 try/catch 구문 제거

--- src/server/services/cron.js
+++ src/server/services/cron.js
@@ -3,9 +3,5 @@ const jnuPoster = require('../utils/jnuPoster');
 
 exports.post = async () => {
   const { userId, userPw, title, contents } = await CronContents.findFirst();
-  try {
-    return await jnuPoster.post({ title, contents, id: userId, pw: userPw });
-  } catch (err) {
-    throw new Error({ type: 'POST ERROR', err });
-  }
+  return jnuPoster.post({ title, contents, id: userId, pw: userPw });
 };
