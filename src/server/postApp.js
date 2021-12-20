5f164fd9332af5437292aac90d3ae19f854768d0
minung.han <hmu332233@gmail.com>
2021-12-20 09:35:49 +0000

    fix: 포스팅 안되던 버그 수정

--- src/server/postApp.js
+++ src/server/postApp.js
@@ -1,9 +1,19 @@
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, './.env') });
 const service = require('./services');
 
-console.log('Start posting..');
-try {
+const mongoose = require('mongoose');
+
+const { sendErrorMessage } = require('./utils/slack');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGO_DB, { useNewUrlParser: true });
+const db = mongoose.connection;
+db.once('open', async () => {
+  console.log('DB connected!');
   const data = await service.cron.post();
-  console.log('Success');
-} catch (err) {
-  console.log(err);
-}
+  db.close();
+});
+db.on('error', err => {
+  console.log('DB ERROR:', err);
+});
