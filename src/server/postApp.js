ac77e8ab104f22bc4d1dc8cafb3f79af6aeaa225
minungHan <hmu332233@gmail.com>
2021-12-20 18:16:03 +0900

    Update postApp.js

--- src/server/postApp.js
+++ src/server/postApp.js
@@ -1,10 +1,9 @@
 const service = require('./services');
 
-(async (req, res, next) => {
-  try {
-    const data = await service.cron.post();
-    console.log('Success');
-  } catch (err) {
-    console.log(err);
-  }
-})();
+console.log('Start posting..');
+try {
+  const data = await service.cron.post();
+  console.log('Success');
+} catch (err) {
+  console.log(err);
+}
